8bc4150359608269c13e9b50e14d7e2946a8a7ba
Vy Nghia <dora.nghia@gmail.com>
2019-07-05 20:53:16 +0700

    update db.php guide

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 require("func.php");
 require("value.php");
-$db = mysqli_connect("localhost","ctjoppmqhosting_vnghia","1151985611") or die("can't connect this database");
-mysqli_select_db($db, "ctjoppmqhosting_test");
+$db = mysqli_connect("localhost","dbuser","db_password") or die("can't connect this database");
+mysqli_select_db($db, "db_name");
 mysqli_set_charset($db, 'utf8');
