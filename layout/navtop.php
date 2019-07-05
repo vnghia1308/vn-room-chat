b0be554a2409fab615b202e0ac1788dee8b01e7f
Vy Nghia <dora.nghia@gmail.com>
2019-07-06 04:37:35 +0700

    fixed menu

--- layout/navtop.php
+++ layout/navtop.php
@@ -7,57 +7,124 @@ if (strpos($_SERVER["SCRIPT_URI"], $filename) !== false) {
 	exit("illegal method");
 }
 ?>
-<nav class="navbar-default navbar-static-side" role="navigation">
-    <div class="sidebar-collapse">
-        <ul class="nav metismenu" id="side-menu">
-            <li class="nav-header">
-                <div class="dropdown profile-element"> 
-				<?php if($inLogin): ?><span><img alt="image" class="img-circle" src="<?= $profilePicture ?>" width="50px" height="50px" /></span><?php endif; ?>
-                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                        <span class="clear"> 
-							<span class="block m-t-xs"> 
-								<strong class="font-bold"><?= $userName ?></strong>
-                             </span> 
-								<!-- <span class="text-muted text-xs block">Art Director <b class="caret"></b></span> -->
-							 </span> 
+<div class="row border-bottom">
+<nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
+<div class="navbar-header">
+	<a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
+	<form role="search" class="navbar-form-custom" action="<?= $_CHAT_FILE ?>">
+		<div class="form-group">
+			<input type="text" placeholder="Join room chat now..." class="form-control" name="room_id">
+		</div>
+	</form>
+</div>
+	<ul class="nav navbar-top-links navbar-right">
+		<li>
+			<span class="m-r-sm text-muted welcome-message">Welcome you to ...</span>
+		</li>
+		<!--
+		<li class="dropdown">
+			<a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
+				<i class="fa fa-envelope"></i>  <span class="label label-warning">16</span>
+			</a>
+			<ul class="dropdown-menu dropdown-messages">
+				<li>
+					<div class="dropdown-messages-box">
+						<a href="profile.html" class="pull-left">
+							<img alt="image" class="img-circle" src="img/a7.jpg">
+						</a>
+						<div class="media-body">
+							<small class="pull-right">46h ago</small>
+							<strong>Mike Loreipsum</strong> started following <strong>Monica Smith</strong>. <br>
+							<small class="text-muted">3 days ago at 7:58 pm - 10.06.2014</small>
+						</div>
+					</div>
+				</li>
+				<li class="divider"></li>
+				<li>
+					<div class="dropdown-messages-box">
+						<a href="profile.html" class="pull-left">
+							<img alt="image" class="img-circle" src="img/a4.jpg">
+						</a>
+						<div class="media-body ">
+							<small class="pull-right text-navy">5h ago</small>
+							<strong>Chris Johnatan Overtunk</strong> started following <strong>Monica Smith</strong>. <br>
+							<small class="text-muted">Yesterday 1:21 pm - 11.06.2014</small>
+						</div>
+					</div>
+				</li>
+				<li class="divider"></li>
+				<li>
+					<div class="dropdown-messages-box">
+						<a href="profile.html" class="pull-left">
+							<img alt="image" class="img-circle" src="img/profile.jpg">
+						</a>
+						<div class="media-body ">
+							<small class="pull-right">23h ago</small>
+							<strong>Monica Smith</strong> love <strong>Kim Smith</strong>. <br>
+							<small class="text-muted">2 days ago at 2:30 am - 11.06.2014</small>
+						</div>
+					</div>
+				</li>
+				<li class="divider"></li>
+				<li>
+					<div class="text-center link-block">
+						<a href="mailbox.html">
+							<i class="fa fa-envelope"></i> <strong>Read All Messages</strong>
+						</a>
+					</div>
+				</li>
+			</ul>
+		</li>
+		<li class="dropdown">
+			<a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
+				<i class="fa fa-bell"></i>  <span class="label label-primary">8</span>
+			</a>
+			<ul class="dropdown-menu dropdown-alerts">
+				<li>
+					<a href="mailbox.html">
+						<div>
+							<i class="fa fa-envelope fa-fw"></i> You have 16 messages
+							<span class="pull-right text-muted small">4 minutes ago</span>
+						</div>
 					</a>
-                    <!-- <ul class="dropdown-menu animated fadeInRight m-t-xs">
-                        <li><a href="profile.html">Profile</a></li>
-                        <li><a href="contacts.html">Contacts</a></li>
-                        <li><a href="mailbox.html">Mailbox</a></li>
-                        <li class="divider"></li>
-                        <li><a href="logout.php">Logout</a></li>
-                    </ul> -->
-                </div>
-                <div class="logo-element">
-                    VN
-                </div>
-            </li>
-			<li class="<?= $homeChatMenu ?>">
-                <a href="<?= $_HOME_FILE ?>"><i class="fa fa-home"></i> <span class="nav-label">Home</span></a>
-            </li>
-			
-			<?php if($inLogin): ?>
-			<li class="<?= $accountMenu ?>">
-                <a href="account.php"><i class="fa fa-user-o"></i> <span class="nav-label">Account</span></a>
-            </li>
-			<li class="<?= $createRoomMenu ?>">
-                <a href="create_room.php"><i class="fa fa-plus"></i> <span class="nav-label">Create room</span></a>
-            </li>
-			
-            <li class="<?= $myRoomMenu ?>">
-				<a href="my_room.php"><i class="fa fa-th-large"></i> <span class="nav-label">My room</span></a>
-            </li>
-			
-			<?php if($isAdmin): ?>
-			<li class="<?= $adminArea ?>">
-                <a href="secret_admin.php"><i class="fa fa-cogs"></i> <span class="nav-label">Admin Area</span></a>
-            </li>
-			<?php endif; ?>
- 			<li>
-                <a href="<?= $_LOGOUT_FILE ?>"><span class="nav-label"><i class="fa fa-sign-out"></i> Logout</span></a>
-            </li>
-			<?php endif; ?>
-        </ul>
-    </div>
+				</li>
+				<li class="divider"></li>
+				<li>
+					<a href="profile.html">
+						<div>
+							<i class="fa fa-twitter fa-fw"></i> 3 New Followers
+							<span class="pull-right text-muted small">12 minutes ago</span>
+						</div>
+					</a>
+				</li>
+				<li class="divider"></li>
+				<li>
+					<a href="grid_options.html">
+						<div>
+							<i class="fa fa-upload fa-fw"></i> Server Rebooted
+							<span class="pull-right text-muted small">4 minutes ago</span>
+						</div>
+					</a>
+				</li>
+				<li class="divider"></li>
+				<li>
+					<div class="text-center link-block">
+						<a href="notifications.html">
+							<strong>See All Alerts</strong>
+							<i class="fa fa-angle-right"></i>
+						</a>
+					</div>
+				</li>
+			</ul>
+		</li>  -->
+
+
+		<li>
+			<a href="logout.php">
+				<i class="fa fa-sign-out"></i> Log out
+			</a>
+		</li>
+	</ul>
+
 </nav>
+</div>
